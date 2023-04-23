5ceb625b2555a9804914f7c05f50c51b95ff51c1
kc <kcmw3e@gmail.com>
2023-04-23 01:56:42 -0400

    added interrupt pin defs

---
+++ arduino/src/rup.h
@@ -0,0 +1,23 @@
+// Casey Walker (2023)
+// 24-671 Electromechanical Systems Design
+// Carnegie Mellon University
+// -------------------------------------------------------------------------- //
+//
+// ui.h
+// for displaying to LCD and handling UI buttons
+//
+
+#ifndef EMSD_RUP_H
+#define EMSD_RUP_H
+
+// Arduino Mega interrupt pins: 2, 3, 18, 19, 20, 21
+enum interrupt_pin {
+  RUP1 = 2,
+  RUP2 = 3,
+  RUP3 = 18,
+  RUP4 = 19,
+  RUP5 = 20,
+  RUP6 = 21,
+};
+
+#endif // EMSD_RUP_H
