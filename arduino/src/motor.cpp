f03e5974488734de582f530f52fa1ee224daf16f
Casey Walker <kcmw3e@gmail.com>
2023-05-01 00:17:59 -0400

    added code for homebrew motor driver

--- arduino/src/motor.cpp
+++ arduino/src/motor.cpp
@@ -5,26 +5,111 @@
 
 #include "motor.h"
 
-Motor::Motor(): _mot(PWM_DIR, MOTOR_PIN_SPD, MOTOR_PIN_DIR)
-{
-  _mot.setSpeed(0);
-}
+#if MOTOR_CONTROL_CYTRON
+  Motor::Motor(): _mot(PWM_DIR, MOTOR_PIN_SPD, MOTOR_PIN_DIR)
+  {
+    _mot.setSpeed(0);
+  }
 
-bool Motor::setup() {
+  bool Motor::setup() {
+    _mot.setSpeed(0);
+    return true;
+  }
 
-  return true;
-}
+  void Motor::write() {
+    _mot.setSpeed(_spd);
+  }
 
-void Motor::write() {
-  _mot.setSpeed(_spd);
-}
+  void Motor::set_spd(int spd) {
+    _spd = spd;
+  }
 
-void Motor::set_spd(int spd) {
-  DEBUG_INFO("Setting speed to %i", spd);
-  _spd = spd;
-}
+  void Motor::stop() {
+    _mot.setSpeed(0);
+    _spd = 0;
+  }
+#else
+  void Motor::_enable() {
+    delayMicroseconds(MOTOR_DISABLE_DELAY); // give relay time to switch
+    digitalWrite(MOTOR_PIN_ENA, MOTOR_ENABLE);
+  }
 
-void Motor::stop() {
-  DEBUG_INFO("Stopping motor.");
-  _mot.setSpeed(0);
-}
+  void Motor::_disable() {
+    digitalWrite(MOTOR_PIN_ENA, MOTOR_DISABLE);
+    delayMicroseconds(MOTOR_DISABLE_DELAY); // give relay time to switch
+  }
+
+  void Motor::_dir(uint8_t dir) {
+    digitalWrite(MOTOR_PIN_DIR, dir);
+  }
+
+  void Motor::_spd(int spd) {
+    // INPUT (analogWrite)    OUTPUT (V_out)
+    // 255                    3.12
+    // 220                    3.08
+    // 200                    2.97
+    // 170                    2.75
+    // 140                    2.35
+    // 120                    1.86
+    // 100                    1.31
+    //  80                    0.89
+    //  60                    0.59
+    //  40                    0.35
+    //  20                    0.16
+    //   0                    0.00
+
+    if (spd > MOTOR_SPD_MAX) spd = MOTOR_SPD_MAX;
+    analogWrite(MOTOR_PIN_SPD, spd);
+  }
+
+  Motor::Motor()
+  {
+    pinMode(MOTOR_PIN_SPD, OUTPUT);
+    pinMode(MOTOR_PIN_DIR, OUTPUT);
+    pinMode(MOTOR_PIN_ENA, OUTPUT);
+
+    stop();
+  }
+
+  bool Motor::setup() {
+    _pend_spd = 0;
+    _write_spd = 0;
+
+    stop();
+
+    return true;
+  }
+
+  void Motor::write() {
+    if (_pend_spd != _write_spd) {
+      if (_pend_spd < 0) {
+        // signs differ, so disable to change direction (to avoid possibility of shorting)
+        if (_write_spd > 0) _disable();
+        _dir(MOTOR_DIR_REV);
+        _spd(-_pend_spd);
+        _enable();
+      } else if (_pend_spd > 0) {
+        // signs differ, so disable to change direction (to avoid possibility of shorting)
+        if (_write_spd < 0) _disable();
+        _dir(MOTOR_DIR_FWD);
+        _spd(_pend_spd);
+        _enable();
+      } else {
+        _disable();
+        _spd(0);
+      }
+    }
+    _write_spd = _pend_spd;
+  }
+
+  void Motor::set_spd(int spd) {
+    _pend_spd = spd;
+  }
+
+  void Motor::stop() {
+    _disable();
+    _spd(0);
+    _pend_spd = 0;
+    _write_spd = 0;
+  }
+#endif // MOTOR_CONTROL_CYTRON
--- arduino/src/motor.h
+++ arduino/src/motor.h
@@ -13,18 +13,49 @@
 #include <Arduino.h>
 #include <Arduino_DebugUtils.h>
 
-#include <CytronMotorDriver.h>
+#define MOTOR_CONTROL_CYTRON 0
 
-#define MOTOR_PIN_SPD 5 // Motor controller input 1
-#define MOTOR_PIN_DIR 6 // Motor controller input 2
+#if MOTOR_CONTROL_CYTRON
+# include <CytronMotorDriver.h>
 
-#define MOTOR_RAISE_SPEED 100
-#define MOTOR_SPLIT_SPEED 100
+# define MOTOR_PIN_SPD 5 // Motor controller input 1
+# define MOTOR_PIN_DIR 6 // Motor controller input 2
+
+# define MOTOR_RAISE_SPEED 100
+# define MOTOR_SPLIT_SPEED 100
+#else
+# define MOTOR_PIN_SPD 4 // pin to control 980Hz PWM motor speed control
+# define MOTOR_PIN_DIR 6 // pin to switch the H-bridge
+# define MOTOR_PIN_ENA 5 // pin to enable motor to move
+
+# define MOTOR_DIR_FWD LOW
+# define MOTOR_DIR_REV HIGH
+
+# define MOTOR_ENABLE LOW
+# define MOTOR_DISABLE HIGH
+
+# define MOTOR_RAISE_SPEED -140
+# define MOTOR_SPLIT_SPEED 140
+
+# define MOTOR_SPD_MAX 140
+
+# define MOTOR_DISABLE_DELAY 10
+#endif // MOTOR_CONTROL_CYTRON
 
 class Motor {
   private:
-    int _spd;
-    CytronMD _mot;
+#   if MOTOR_CONTROL_CYTRON
+      int _spd;
+      CytronMD _mot;
+#   else
+    int _pend_spd;
+    int _write_spd;
+
+    void _enable();
+    void _disable();
+    void _dir(uint8_t dir);
+    void _spd(int spd);
+#   endif // MOTOR_CONTROL_CYTRON
   public:
     Motor();
     bool setup();
